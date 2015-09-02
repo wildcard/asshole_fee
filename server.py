92af0ced80d2fea938cb6990889b0cab973446d0
Uri Goren <uri@bla.sh>
2015-09-02 20:51:06 +0300

    Added a very basic server

--- server.py
+++ server.py
@@ -1,3 +1,25 @@
 #TODO: get plate-number and curse-word from user and save with a timestamp
 #TODO: list fee per user
 #TODO: pay and reset debt
+
+from flask import Flask
+app = Flask(__name__)
+
+db = {}
+
+@app.route('/')
+def index():
+    return 'Nothing to see here, carry on...'
+
+@app.route('/asshole/<lpr>')
+def fine_as_asshole(lpr):
+    print (lpr)
+    if lpr in db.keys():
+        db[lpr]+=1
+    else:
+        db[lpr]=1
+    return str(db[lpr])
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

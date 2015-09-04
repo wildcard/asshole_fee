1f8d3663fa8244fff48a12a653ab5dae6ad663f8
alex <salexln@gmail.com>
2015-09-04 11:42:42 +0300

    small changes

--- license_plate.py
+++ license_plate.py
@@ -10,14 +10,14 @@ def detect(image):
     lines = out.splitlines()
     res_num = lines[0][8:10]
 
-    licence_plate = lines[1][6:12] #can we have bigger licence plate??
+    license_plate = lines[1][6:12] #can we have bigger licence plate??
     accuracy = lines[1][24:]
 
-    print 'Best results (out of %s) with accuracy %s is: %s' %(res_num, accuracy, licence_plate)
+    print 'Best results (out of %s) with accuracy %s is: %s' %(res_num, accuracy, license_plate)
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
-        print "Usage: python licence_plate.py image.jpg"
+        print "Usage: python license_plate.py image.jpg"
         sys.exit(-1)
     detect(sys.argv[1])

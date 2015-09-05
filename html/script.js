5cd87e860dfaae71f92d8a3bd6af76b740188924
Uri Goren <uri@bla.sh>
2015-09-05 22:11:27 +0300

    styled points

--- html/script.js
+++ html/script.js
@@ -1,9 +1,16 @@
 function update_status()
 {
+	var template_row='<div class="row">';
+	template_row+='<div class="col-md-8"><p class="reg">$lp$</p></div>';
+	//template_row+='<div class="col-md-4">$points$</div>';
+	template_row+='<div class="col-md-4"><div class="stopsign"><span class="stopword">$points$ </span> </div></div>';
+	template_row+='</div>\n';
+	
 	jQuery.getJSON('/list',function (data) {
-		jQuery('#status').empty();
-		jQuery.each(data,function (key,value) {
-			jQuery( "#status" ).append( '<div class="row"><div class="col-md-8"><p class="reg">'+key+'</p></div><div class="col-md-4">'+value+'</div></div>' );
+		var status_box=jQuery('#status');
+		status_box.empty();
+		jQuery.each(data,function (key,value) {			
+			status_box.append( template_row.replace('$lp$',key).replace('$points$',value));
 		})
 	});
 	setTimeout(update_status, 3000);

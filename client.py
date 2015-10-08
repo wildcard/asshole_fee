f4b629a34f661a33657d0e91d8eb51cbdc6ada7f
alex <salexln@gmail.com>
2015-10-08 20:04:31 +0300

    latest chages

--- client.py
+++ client.py
@@ -1,7 +1,10 @@
 import urllib2
 import subprocess
 import os
+import time
+import sys
 
+import readchar
 import speech_recognition as sr
 import pygame.camera
 import pygame.image
@@ -33,7 +36,7 @@ def capture_image(path):
 
 iteration_count = 1000
 server = 'http://127.0.0.1:5000'
-curses = ['bitch', 'beach', 'biatch', 'shit', 'ass', '*']
+curses = ['bitch', 'beach', 'biatch', 'shit', 'ass', '*', 'damn', 'bloody', 'jerk']
 
 
 pygame.camera.init()
@@ -41,59 +44,64 @@ cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
 cam.start()
 colorama.init()
 
+while True:
+    for i in range(iteration_count):
+        points = 0
 
-for i in range(iteration_count):
-    points = 0
+        path = 'plates/new_plate.jpg'
+        print i
+        capture_image(path)
 
-    path = 'plates/new_plate.jpg'
-    print i
-    capture_image(path)
+        lpr = detect_lpr(path)
+        if lpr != 'No license plates found':
+            print colorama.Fore.YELLOW + lpr + colorama.Fore.WHITE
+            break
 
-    lpr = detect_lpr(path)
-    if lpr != 'No license plates found':
-        print colorama.Fore.YELLOW + lpr + colorama.Fore.WHITE
-        break
+        print colorama.Fore.RED + lpr + colorama.Fore.WHITE
 
-    print colorama.Fore.RED + lpr + colorama.Fore.WHITE
+    # obtain audio from the microphone
+    print colorama.Fore.BLACK
+    r = sr.Recognizer()
 
-# obtain audio from the microphone
-print colorama.Fore.BLACK
-r = sr.Recognizer()
+    if DBG:
+        x = readchar.readchar()
+        speech =''
+        if x == '1':
+            speech = 'what a jerk'
+        elif x == '2':
+            speech = 'god damn it'
+        elif x == '3':
+            speech = 'bloody hell'
+        else:
+            print colorama.Fore.RED + 'could not recognize' + colorama.Fore.WHITE
+        time.sleep(2)
 
-if DBG:
-    x = input()
-    speech =''
-    if x == 1:
-        speech = 'that jerk cut me off'
-    elif x == 2:
-        speech = 'god damn it'
-    elif x == 3:
-        speech = 'bloody hell'
-    else:
-        print colorama.Fore.RED + 'could not recognize' + colorama.Fore.WHITE
-
-
-
-    print colorama.Fore.GREEN + speech + colorama.Fore.WHITE
-else:
-    with sr.Microphone() as source:
-        print colorama.Fore.WHITE
-        print('Say something!')
-        audio = r.listen(source)
-        print('Done listening!')
-    speech = ''
-    try:
-        speech = r.recognize_google(audio)
         print colorama.Fore.GREEN + speech + colorama.Fore.WHITE
-    except sr.UnknownValueError:
-        print('Google Speech Recognition could not understand audio')
-    except sr.RequestError:
-        print('Could not request results from Google Speech Recognition service')
+    else:
+        with sr.Microphone() as source:
+            print colorama.Fore.WHITE
+            print('Say something!')
+            audio = r.listen(source)
+            print('Done listening!')
+        speech = ''
+        try:
+            speech = r.recognize_google(audio)
+            print colorama.Fore.GREEN + speech + colorama.Fore.WHITE
+        except sr.UnknownValueError:
+            print('Google Speech Recognition could not understand audio')
+        except sr.RequestError:
+            print('Could not request results from Google Speech Recognition service')
 
-points = len([c for c in curses if speech.find(c) > - 1])
-# print (points)
-if points > 0:
-    urllib2.urlopen('{s}/asshole/{l}'.format(s=server,l=lpr)).read()
+    points = len([c for c in curses if speech.find(c) > - 1])
+    # print (points)
+    if points > 0:
+        urllib2.urlopen('{s}/asshole/{l}'.format(s=server,l=lpr)).read()
+
+    cam.stop()
+    time.sleep(1)
+    subprocess.call("cp plates/black.jpg plates/new_plate.jpg", shell=True)
+    cam.start()
+
 
 cam.stop()
 pygame.camera.quit()
